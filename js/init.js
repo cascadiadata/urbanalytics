57983562b33d067b9918ad5c82e3f69767cf25fe
Peter Lin <peterlin9863@gmail.com>
2017-12-10 21:48:53 -0800

    Adding blog functionality

--- js/init.js
+++ js/init.js
@@ -9,13 +9,13 @@
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			global: { href: 'css/style.css', containers: 1400, grid: { gutters: ['2em', 0] } },
-			xxlarge: { media: '(max-width: 3000px)', href: 'css/style-xxlarge.css', containers: 1400 },
-			xlarge: { media: '(max-width: 1680px)', href: 'css/style-xlarge.css', containers: 1200 },
-			large: { media: '(max-width: 1280px)', href: 'css/style-large.css', containers: 960, grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
-			medium: { media: '(max-width: 980px)', href: 'css/style-medium.css', containers: '90%', grid: { zoom: 2 } },
-			small: { media: '(max-width: 736px)', href: 'css/style-small.css', containers: '90%!', grid: { gutters: ['1.25em', 0], zoom: 3 } },
-			xsmall: { media: '(max-width: 480px)', href: 'css/style-xsmall.css' }
+			global: { href: '/css/style.css', containers: 1400, grid: { gutters: ['2em', 0] } },
+			xxlarge: { media: '(max-width: 3000px)', href: '/css/style-xxlarge.css', containers: 1400 },
+			xlarge: { media: '(max-width: 1680px)', href: '/css/style-xlarge.css', containers: 1200 },
+			large: { media: '(max-width: 1280px)', href: '/css/style-large.css', containers: 960, grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
+			medium: { media: '(max-width: 980px)', href: '/css/style-medium.css', containers: '90%', grid: { zoom: 2 } },
+			small: { media: '(max-width: 736px)', href: '/css/style-small.css', containers: '90%!', grid: { gutters: ['1.25em', 0], zoom: 3 } },
+			xsmall: { media: '(max-width: 480px)', href: '/css/style-xsmall.css' }
 		},
 		plugins: {
 			layers: {
